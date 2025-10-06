b4ae7d999cacce00e0b6b9f1bef35b242736cd75
md.omar faruck <omarfaruck1994@gmail.com>
2025-10-06 08:06:51 +0600

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/js/app.js')
+    @inertiaHead
+     <link rel="stylesheet" href="
+ {{url('https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.
+ css')}}" />
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
